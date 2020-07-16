cc0eb7210aea12810a548932839ceeb697a143d2
Mangesh Jadhav <149mpj@gmail.com>
2020-07-17 02:43:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ReactiveFormWithValidation';
+  status;
+  data;
+
+  constructor(private fb:FormBuilder){}
+
+  profileForm=this.fb.group({
+    fullname: ['',[Validators.required,Validators.minLength(5),Validators.maxLength(35)]],
+    email: ['',[Validators.required,Validators.email]],
+    mobile:['',[Validators.required,Validators.minLength(10)]],
+  });
+
+  Submitdata(profileForm){
+    // console.log(profileForm.value);
+    this.status=true;
+    this.data = profileForm.value;
+    // console.log(profileForm.controls);
+    // console.log(profileForm.controls.fullname.value);
+    // console.log(profileForm.controls.email.value);
+    // console.log(profileForm.controls.mobile.value);
+    profileForm.reset();
+  }
+
+
 }
